0f9ab35c831424ac68d5963bcd69acd7e524ea20
whemurad <whemurad@3fa4fd52-b77f-11de-97ce-e783fd196cab>
2009-11-30 03:20:15 +0000

    UI Changes

---
+++ trunk/classes/Users.class.php
@@ -0,0 +1,55 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Usersclass
+ *
+ * @author William
+ */
+class Usersclass {
+    public function getAdvancedUserHTML($IMthreshold='1.0', $TMLthreshold='2.0', $ScreeningThreshold='2.0', $IntensityLimit='0.03', $CMthreshold='1.0'){
+        
+        return '<table class="advancedusers">
+                <tr class="advancedusers">
+                    <td class="advancedusersleft">Initial Match Threshold:</td>
+                    <td class="advancedusersright">
+                        <input type="text" id="IMthreshold" name="IMthreshold" size="5" value="'.$IMthreshold.'"></input>
+                        * (default: +-1.0)
+                    </td>
+                </tr>
+                <tr class="advancedusers">
+                    <td class="advancedusersleft">TML Formation Threshold:</td>
+                    <td class="advancedusersright">
+                        <input type="text" id="TMLthreshold" name="TMLthreshold" size="5" value="'.$TMLthreshold.'"></input>
+                        * expand m/z values according to precursor charge state (default: 2.0)
+                    </td>
+                </tr>
+                <tr class="advancedusers">
+                    <td class="advancedusersleft">m/z Screening Threshold:</td>
+                    <td class="advancedusersright">
+                        <input type="text" id="ScreeningThreshold" name="ScreeningThreshold" size="5" value="'.$ScreeningThreshold.'"></input>
+                        * selecting a median value for similiar m/z values (default: 2.0)
+                    </td>
+                </tr>
+                <tr class="advancedusers">
+                    <td class="advancedusersleft">m/z Intensity Limit:</td>
+                    <td class="advancedusersright">
+                        <input type="text" id="IntensityLimit" name="IntensityLimit" size="5" value="'.$IntensityLimit.'"></input>
+                        * lowest m/z intensity accepted (default: 0.03 or 3%)
+                    </td>
+                </tr>
+                <tr class="advancedusers">
+                    <td class="advancedusersleft">Confirmed Threshold:</td>
+                    <td class="advancedusersright">
+                        <input type="text" id="CMthreshold" name="CMthreshold" size="5" value="'.$CMthreshold.'"></input>
+                        * (default: +-1.0)
+                    </td>
+                </tr>
+                <tr><td colspan="2"><p></p></td></tr>
+              </table>';
+    }
+}
+?>
