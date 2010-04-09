183eb4b492b08e895d44383a05c90b5047f54043
whemurad <whemurad@3fa4fd52-b77f-11de-97ce-e783fd196cab>
2010-04-09 02:59:31 +0000

    Latest updates

---
+++ trunk/CHI/index.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>CHI-Cultural Homestay International</title>
+<link href="css/chinet.css" rel="stylesheet" type="text/css" />
+<script src="SpryAssets/SpryMenuBar.js" type="text/javascript"></script>
+<script type="text/javascript">
+
+
+
+<!--
+
+
+
+function MM_preloadImages() { //v3.0
+
+
+
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+
+
+
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+
+
+
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+
+
+
+}
+
+
+
+//-->
+
+
+
+</script>
+<link href="css/SpryMenuBarHorizontal.css" rel="stylesheet" type="text/css" />
+<script src="Scripts/AC_RunActiveContent.js" type="text/javascript"></script>
+</head>
+<body>
+<table width="965" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td><table width="965" border="0" cellspacing="0" cellpadding="0">
+        <tr>
+          <td height="89"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td width="78%" valign="top"><img src="images/logo.gif" width="474" height="89" /></td>
+                <td width="22%" align="right" valign="bottom"><table width="100%" border="0" cellspacing="0" cellpadding="0" style="margin:0 20px 20px 0;">
+                  <tr>
+                    <td align="right"><span class="date">Wednesday,Aug 13, 2009<br />
+                      11:55:00 AM PST</span></td>
+                  </tr>
+                </table></td>
+              </tr>
+            </table></td>
+        </tr>
+      </table></td>
+  </tr>
+  <tr>
+    <td class="p_top15"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td width="960" valign="top" bgcolor="#FFFFFF"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+            <tr>
+              <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                  <tr>
+                    <td width="88%"><div id="topmenu">
+                        <ul id="MenuBar1" class="MenuBarHorizontal">
+                          <li id="Home"><a class="MenuBarItemSubmenu" href="#">Home</a></li>
+                          <li id="Programs_1"><a href="#" class="MenuBarItemSubmenu">Programs in the US </a>
+                              <ul class="rr">
+                                <li><a href="#">Academic Year</a></li>
+                                <li><a href="#">Au Pair USA</a></li>
+                                <li><a href="#">Camp USA</a></li>
+                                <li><a href="#">Group Homestay</a></li>
+                                <li><a href="#">Hospitality</a></li>
+                                <li><a href="#">Internship / Training</a></li>
+                                <li><a href="#">Short Term Enrichment</a></li>
+                                <li><a href="#">Work & Travel USA</a></li>
+                              </ul>
+                          </li>
+                          <li id="Programs_2"><a class="MenuBarItemSubmenu" href="#">Programs Abroad </a>
+                              <ul>
+                                <li><a href="#">High School Abroad</a></li>
+                                <li><a href="#">Work & Travel Abroad</a></li>
+                              </ul>
+                          </li>
+                          <li id="Host"><a class="MenuBarItemSubmenu" href="#">Become a Host Family</a> </li>
+                          <li id="Employers"><a href="#" class="MenuBarItemSubmenu">Become a Host Business </a> </li>
+                        </ul>
+                    </div></td>
+                    <td width="12%"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                        <tr>
+                          <td align="right"  ><a href="http://twitter.com/CHI_News" target="_blank"><img src="images/twitter.gif" width="40" height="37" border="0" /></a></td>
+                          <td><a href="http://www.facebook.com/CulturalHomestayInternational" target="_blank"><img src="images/facebook.gif" width="40" height="36" border="0" /></a></td>
+                        </tr>
+                        <tr></tr>
+                    </table></td>
+                  </tr>
+              </table></td>
+            </tr>
+            <tr>
+              <td><?php
+            		$randnum = rand(1,4);
+               	 ?>
+                         <img src="<?php echo 'images/main0'.$randnum.'.jpg';?>" width="960" height="273" /></td>
+            </tr>
+            <tr>
+              <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                  <tr>
+                    <td class="conteants"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                        <tr>
+                          <td width="512"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                              <tr>
+                                <td width="20">&nbsp;</td>
+                                <td width="20"><img src="images/conteants_bar.gif" width="4" height="219" /></td>
+                                <td valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                    <tr>
+                                      <td><a href="#"><img src="images/title_chinews.gif" width="100" height="31" border="0" /></a></td>
+                                    </tr>
+                                    <tr>
+                                      <td><table border="0" cellspacing="0" cellpadding="0">
+                                          <tr>
+                                            <td width="248"><table border="0" cellspacing="0" cellpadding="0">
+                                                <tr>
+                                                  <td class="tite_1">CHI Au Pair USA Offers Their <br />
+                                                    Own Economic Stimulus Plan!</td>
+                                                </tr>
+                                                <tr>
+                                                  <td><table width="92%" border="0" cellspacing="0" cellpadding="0">
+                                                      <tr>
+                                                        <td valign="top"><img src="images/pic_news.gif" width="93" height="111" align="top" /></td>
+                                                        <td class="txt_gray">CHI Au Pair USA is introducing a few temporary promotions for both coordinators as well as host families!</td>
+                                                      </tr>
+                                                  </table></td>
+                                                </tr>
+                                                <tr>
+                                                  <td align="right" class="read"><a href="#">READ MORE</a><img src="images/arr-1.gif" width="6" height="9" /></td>
+                                                </tr>
+                                            </table></td>
+                                            <td valign="top" class="tite_1" ><span class="new_blog"><img src="images/arr_2.gif" width="11" height="6" /> View Our News Blog</span><br />
+                                                <br />
+                                              <br />
+                                                <ul>
+                                                  <li><a href="#"><span class="dot">&bull;</span> Expand Your Horizons Video &nbsp;&nbsp;Contest!</a></li>
+                                                  <br />
+                                                  <li><a href="#"><span class="dot">&bull;</span> Participant Spotlight: Eugen </a></li>
+                                                  <br />
+                                                  <li><a href="#"><span class="dot">&bull;</span> Travel Abroad for Americans in   &nbsp;&nbsp;Japan!</a> <a href="#"></a></li>
+                                                </ul></td>
+                                          </tr>
+                                      </table></td>
+                                    </tr>
+                                </table></td>
+                              </tr>
+                          </table></td>
+                          <td width="460"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                              <tr>
+                                <td>&nbsp;</td>
+                                <td width="20"><img src="images/conteants_bar.gif" width="4" height="219" /></td>
+                                <td valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                    <tr>
+                                      <td><a href="#"><img src="images/title_ourprograms.gif" width="185" height="31" border="0" /></a></td>
+                                    </tr>
+                                    <tr>
+                                      <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                          <tr>
+                                            <td style="padding-top:10px"><script type="text/javascript">
+
+AC_FL_RunContent( 'codebase','http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,28,0','width','400','height','181','title','gellary','src','FLASH/Gallery','quality','high','pluginspage','http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash','movie','FLASH/Gallery' ); //end AC code
+
+    </script>
+                                                <noscript>
+                                                <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,28,0" width="400" height="181" title="gellary">
+                                                  <param name="movie" value="FLASH/Gallery.swf" />
+                                                  <param name="quality" value="high" />
+                                                  <embed src="FLASH/Gallery.swf" quality="high" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="400" height="181"></embed>
+                                                </object>
+                                              </noscript></td>
+                                          </tr>
+                                      </table></td>
+                                    </tr>
+                                </table></td>
+                              </tr>
+                          </table></td>
+                        </tr>
+                    </table></td>
+                  </tr>
+                  <tr>
+                    <td class="foot_address"><table width="920" border="0" align="center" cellpadding="0" cellspacing="0">
+                        <tr>
+                          <td class="addrees_Red"><div align="left"><span class="about"><a href="#">About Us</a></span> | <span class="about"><a href="#">Contact Us </a></span></div></td>
+                        </tr>
+                    </table></td>
+                  </tr>
+              </table></td>
+            </tr>
+        </table></td>
+        <td width="10"><img src="images/bg_right_1.gif"  width="5" height="610"/></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td align="center" class="foot"><table width="920" border="0" cellspacing="0" cellpadding="0">
+        <tr>
+          <td align="left" valign="top" class="copy">© Cultural Homestay International OPENING DOORS TO THE WORLD SINCE 1980</td>
+          <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td></td>
+              </tr>
+              <tr>
+                <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tr>
+                      <td valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                          <tr align="right">
+
+                            <td width="60%"><a href="#"><img src="images/foot_logo1.gif" width="88" height="48" border="0" /></a></td>
+                            <td width="40%" ><a href="#"><img src="images/foot_logo2.gif" width="110" height="58" border="0"/></a></td>
+
+                          </tr>
+
+                      </table></td>
+                    </tr>
+
+                    <tr>
+                      <td align="right" class="deo"><a href="http://www.deographics.com/new-york-web-design.php">new york website design</a> by <a href="http://www.deographics.com/"> Deographics</a></td>
+                    </tr>
+                </table></td>
+              </tr>
+            </table></td>
+        </tr>
+      </table></td>
+  </tr>
+</table>
+<br />
+<br />
+<script type="text/javascript">
+
+
+
+<!--
+
+
+
+var MenuBar1 = new Spry.Widget.MenuBar("MenuBar1", {imgDown:"../SpryAssets/SpryMenuBarDownHover.gif", imgRight:"../SpryAssets/SpryMenuBarRightHover.gif"});
+
+
+
+//-->
+
+
+
+</script>
+</body>
+</html>
