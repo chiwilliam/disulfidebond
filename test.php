9ecba317c4f6ce0ea1c2cfb2ec2934b0f46a66af
whemurad <whemurad@3fa4fd52-b77f-11de-97ce-e783fd196cab>
2010-05-09 07:25:05 +0000

    Tintin issues

--- test.php
+++ test.php
@@ -1,7 +1,8 @@
 <?php
     echo $_FILES["zipFile"];
     $file = $_FILES["zipFile"];
-    echo $file['tmp_name'];
+    echo $file['tmp_name'].'<br />';
+    echo $file['name'].'<br />';
     $zip = zip_open($file["tmp_name"]);
     //echo 'test...';
 ?>
