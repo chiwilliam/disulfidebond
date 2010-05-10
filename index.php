72410da80d2f609b224899d9fe82a28dd68ba0ea
whemurad <whemurad@3fa4fd52-b77f-11de-97ce-e783fd196cab>
2010-05-10 02:08:11 +0000

    Tintin fixes

--- index.php
+++ index.php
@@ -88,7 +88,9 @@
                         <tr>
                             <td colspan="2" class="centered">
                                 <!-- Youtube tutorial vide -->
-                                
+                                <p>
+                                    <object width="425" height="344"><param name="movie" value="http://www.youtube.com/v/h0noOupSVW0&hl=en&fs=1"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/h0noOupSVW0&hl=en&fs=1" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="425" height="344"></embed></object>
+                                </p>
                             </td>
                         </tr>
                     </table>
