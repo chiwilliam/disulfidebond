12950b0c1c48fce55253f2b7078e88d5edfc79ad
whemurad <whemurad@3fa4fd52-b77f-11de-97ce-e783fd196cab>
2010-05-12 22:43:36 +0000

    Data Analysis fixes

--- trunk/test.php
+++ trunk/test.php
@@ -2,9 +2,10 @@
     $file = $_FILES["zipFile"];
     echo $file['tmp_name'].'<br />';
     echo $file['name'].'<br />';
+    echo 'Working folder: '.getcwd().'<br />';
     $zip = zip_open($file["tmp_name"]);
     echo $zip.'<br />';
-    echo 'Working folder: '.getcwd();
+    
 ?>
 
 
