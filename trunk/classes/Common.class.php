0590a153d90eb27188c1fee5912f071f825573c1
whemurad <whemurad@3fa4fd52-b77f-11de-97ce-e783fd196cab>
2010-06-23 05:20:33 +0000

    PP-values

--- trunk/classes/Common.class.php
+++ trunk/classes/Common.class.php
@@ -573,6 +573,13 @@ class Commonclass {
         return bcmul($value, $this->factorial(bcsub($value, '1'), $scale), $scale);
     }
 
+    private function factorial2($value){
+        if($value == 0){
+            return 1;
+        }
+        return $value*$this->factorial($value-1);
+    }
+
     public function calculatePPvalue($TML, $CM, $CMthreshold, $detectionrange){
 
         $totalIons = count($TML);
@@ -582,9 +589,11 @@ class Commonclass {
         for($i=$numMatches;$i<=$totalIons;$i++){
             //$tmp = $this->factorial($totalIons);
             //$tmp2 = $this->factorial($i);
+            $tmp = $this->factorial2($totalIons);
+            $tmp2 = $this->factorial2($i);
             //$tmp2 = bcmul($tmp2,$this->factorial(($totalIons-$i)),500);
             //$tmp = bcdiv($tmp,$tmp2,500);
-            $tmp = bcdiv(10000000000000000,10000000000000000,500);
+            $tmp = $tmp/$tmp2;
             $tmp = $tmp*(pow($p2,$i));
             $tmp = $tmp*(pow((1.0-$p2),($totalIons-$i)));
             $alfa += $tmp;
