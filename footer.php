49c5544d88b81696686aaf8259a748af7024de78
whemurad <whemurad@3fa4fd52-b77f-11de-97ce-e783fd196cab>
2010-09-24 07:44:06 +0000

    Zoomsumer changes

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <div id="footer">
     <p>MS2DB+ was partially supported by funding from the National Science Foundation (NSF) under grant IIS-0644418 (CAREER).</p>
-    <p><a target="_blank" href="copyright.php">Copyright © 2010</a> William Murad and Rahul Singh. All Rights Reserved.</p>
+    <p><a href="copyright.php">Copyright © 2010</a> William Murad and Rahul Singh. All Rights Reserved.</p>
 </div>
 <div id="footerbottom">
     <img src="images/footerbottom.png" />
