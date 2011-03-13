6e52e4eacdcadba6d6f60356504ad0fb3bfe29bc
whemurad <whemurad@3fa4fd52-b77f-11de-97ce-e783fd196cab>
2011-03-13 18:28:46 +0000

    2011 modifications

--- kernel.php
+++ kernel.php
@@ -8,7 +8,7 @@
     $root = $_SERVER['DOCUMENT_ROOT'];
    
     //fix for tintin root path
-    if(trim($root) == "/var/www/html/bioinformatics"){
+    if(trim($root) == "/var/www/html/bioinformatics" || trim($root) == "/var/www"){
         //for tintin
         $root = "/home/whemurad/public_html";
         //for haddock2
