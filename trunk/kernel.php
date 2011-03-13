9c2aed1d1bbe5782d8f335b43e295a50a0c96718
whemurad <whemurad@3fa4fd52-b77f-11de-97ce-e783fd196cab>
2011-03-13 18:26:08 +0000

    2011 modifications

--- trunk/kernel.php
+++ trunk/kernel.php
@@ -19,9 +19,6 @@
     //remove time limit when executing a script
     set_time_limit(0);
     
-    echo 'test';
-    exit;
-    
     //include necessary classes
     require_once $root."/disulfidebond/classes/Users.class.php";
     require_once $root."/disulfidebond/classes/AA.class.php";
@@ -30,6 +27,9 @@
     require_once $root."/disulfidebond/classes/ConfirmedMatch.class.php";
     require_once $root."/disulfidebond/classes/Charting.class.php";
     require_once $root."/disulfidebond/prediction.php";
+    
+    echo 'test';
+    exit;
     
     //initialize objects
     $Users = new Usersclass();
