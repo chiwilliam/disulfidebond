bd2b481655b5d079bec131e073fa2fb6b5a044a8
whemurad <whemurad@3fa4fd52-b77f-11de-97ce-e783fd196cab>
2011-03-13 18:24:33 +0000

    2011 modifications

--- trunk/kernel.php
+++ trunk/kernel.php
@@ -245,6 +245,9 @@
         $delta = 0;
     }
     
+    echo 'test';
+    exit;
+    
     if(strlen($_FILES["zipFile"]["name"]) > 0 && $_FILES["zipFile"][size] > 0 &&
        ($_FILES["zipFile"]["type"] == "application/zip" || $_FILES["zipFile"]["type"] == "application/x-zip-compressed" || $_FILES["zipFile"]["type"] == "application/octet-stream") &&
        $fastaProtein != false &&
