fa0bb08458219c6ac2091dd30e549f46fd4736e6
whemurad@gmail.com <whemurad@gmail.com@3fa4fd52-b77f-11de-97ce-e783fd196cab>
2011-04-27 16:42:02 +0000

    MS2DB++ fixes to haddock

--- trunk/header.php
+++ trunk/header.php
@@ -7,7 +7,7 @@
         -->
     </div>
     <div id="masterheadcenter">
-        Efficient Disulfide Bond Determination Using Mass Spectrometry
+        Efficient Disulfide Bond Determination Using Multiple Methods
     </div>
     <div id="mastheadright">
         <a href="http://www.sfsu.edu" class="noborders" target="_blank"><img alt="SFSU Logo" src="images/sfsu.jpg" onmouseover="Tip('SFSU http://www.sfsu.edu')" onmouseout="UnTip()"/></a>
