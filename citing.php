cb733b44de29e20fb3f618b845b1ca7c77e76260
whemurad@gmail.com <whemurad@gmail.com@3fa4fd52-b77f-11de-97ce-e783fd196cab>
2014-05-08 01:42:11 +0000

    Updating top menu to add citing us and updating publications

---
+++ citing.php
@@ -0,0 +1,80 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html dir="ltr" xmlns="http://www.w3.org/1999/xhtml">
+    <!-- #BeginTemplate "master.dwt" -->
+    <head>
+        <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+        <!-- #BeginEditable "doctitle" -->
+        <title>Publications</title>
+        <!-- #EndEditable -->
+        <link href="styles/style1.css" media="screen" rel="stylesheet" title="CSS" type="text/css" />
+        <link href="styles/style.css" rel="stylesheet" type="text/css" />
+        <script type="text/javascript" src="js/wz_jsgraphics.js"></script>
+    </head>
+    <body>
+        <?php include_once("analyticstracking.php") ?>
+        <script type="text/javascript" src="js/wz_tooltip.js"></script>
+        <!-- Begin Container -->
+        <div id="container">
+            <!-- Begin Header -->
+            <?php include "header.php" ?>
+            <!-- End Header -->
+            <!-- Begin Navigation -->
+            <?php include 'menu.php';?>
+            <!-- End Navigation -->
+            <!-- Begin Page Content -->
+            <div id="page_content">
+                <!-- Begin Left Column -->
+                <div id="column_l">
+                    <!-- #BeginEditable "content" -->
+                    <table class="content" id="publications" cellpadding="0" cellspacing="0" summary="" width="80%">
+                        <tr>
+                            <td>
+                                <br></br>
+                                <p><span style="font-size:14px;font-weight:bold;">Please cite the following references for MS2DB++:</span></p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <ul>
+                                    <li>
+                                        <font face="Times New Roman" size="3">
+                                        R. Singh and W. Murad, "Protein disulfide topology determination through the fusion of mass 
+                                        spectrometric analysis and sequence-based prediction using Dempster-Shafer theory", 
+                                        <i>BMC Bioinformatics</i>, 14(Suppl 2):S20, 2013
+                                        </font>
+                                        <a target="_blank" href="http://www.biomedcentral.com/1471-2105/14/S2/S20">
+                                            <img alt="[PDF]" border="0" height="16" src="./images/pdf.gif" width="16" />
+                                        </a>
+                                    </li>
+                                    <dd>&nbsp;</dd>
+                                    <li>
+                                        <font face="Times New Roman" size="3">
+                                        W. Murad and R. Singh, "The MS2DB++ Webserver: Disulfide Bond Determination 
+                                        Through Evidence Combination", 
+                                        <i>IEEE Transactions on NanoBioscience</i>, 12, Issue 4, pp.340-342, 2014
+                                        </font>
+                                        <a target="_blank" href="http://ieeexplore.ieee.org/xpl/articleDetails.jsp?arnumber=6695758">
+                                            <img alt="[PDF]" border="0" height="16" src="./images/pdf.gif" width="16" />
+                                        </a>
+                                    </li>                                    
+                                </ul>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p></p>
+                            </td>
+                        </tr>
+                    </table>
+                    <!-- #EndEditable "content" -->
+                </div>
+                <!-- End Left Column -->
+            </div>
+            <!-- End Page Content -->
+            <!-- Begin Footer -->
+            <?php include "footer.php" ?>
+            <!-- End Footer -->
+        </div><!-- End Container -->
+    </body>
+    <!-- #EndTemplate -->
+</html>
